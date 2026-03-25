39258786b44f89f5a5dab09d7e5fdfb9e0098ac5
surikaviya3-hue <surikaviya3@gmail.com>
2026-03-25 11:57:53 +0530

    Added Dockerfile and updated backend config

--- backend/src/main/java/jar/DemoApplication.java
+++ backend/src/main/java/jar/DemoApplication.java
@@ -3,10 +3,15 @@ package jar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
